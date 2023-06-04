4502482232e814ea6be48846ce7bbd7c48fcf4ff
David Wildasin <dragonmaus@posteo.net>
2023-06-04 00:28:15 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,19 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.serialization")
+	kotlin("jvm")
+	kotlin("plugin.serialization")
 
-    id("org.jlleitschuh.gradle.ktlint")
+	id("org.jlleitschuh.gradle.ktlint")
 
-    application
+	application
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.3.2")
-    implementation("com.sksamuel.hoplite", "hoplite-core", "2.1.1")
+	implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.3.2")
+	implementation("com.sksamuel.hoplite", "hoplite-core", "2.1.1")
 }
 
 internal val projectGroup: String by project
@@ -28,36 +26,32 @@ application.mainClass.set("us.dragonma.minecraft.server.launcher.MainKt")
 internal val javaVersion: String by project
 
 java {
-    val javaVersion = JavaVersion.toVersion(javaVersion)
+	val javaVersion = JavaVersion.toVersion(javaVersion)
 
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(javaVersion.toString().toInt()))
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
-    withSourcesJar()
+	sourceCompatibility = javaVersion
+	targetCompatibility = javaVersion
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(javaVersion.toString().toInt()))
+		sourceCompatibility = javaVersion
+		targetCompatibility = javaVersion
+	}
+	withSourcesJar()
 }
 
-tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        options.release.set(javaVersion.toInt())
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
+tasks.compileJava {
+	options.encoding = "UTF-8"
+	options.release.set(javaVersion.toInt())
+	sourceCompatibility = javaVersion
+	targetCompatibility = javaVersion
+}
 
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = javaVersion
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-    }
+tasks.compileKotlin {
+	kotlinOptions.jvmTarget = javaVersion
+}
 
-    jar {
-        configurations["compileClasspath"].forEach { from(zipTree(it.absoluteFile)) }
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
-        from("LICENSE") { rename { "$it.${project.name}" } }
-        manifest.attributes["Main-Class"] = application.mainClass
-    }
+tasks.jar {
+	configurations["compileClasspath"].forEach { from(zipTree(it.absoluteFile)) }
+	duplicatesStrategy = DuplicatesStrategy.INCLUDE
+	from("LICENSE") { rename { "$it.${project.name}" } }
+	manifest.attributes["Main-Class"] = application.mainClass
 }
